2a372a2ad1a94253c6defab00bae5cb222115ca8
MatiSapino <msapino@itba.edu.ar>
2025-10-02 09:46:42 -0300

    Feat: LSBI method

--- src/stegobmp.c
+++ src/stegobmp.c
@@ -147,6 +147,33 @@ static int lsb4_hide(BMP *bmp, const unsigned char *payload_buffer, const size_t
     return 0;
 }
 
-static int lsbI_hide(BMP *bmp, const unsigned char *payload_buffer, size_t payload_size) {
+static int lsbI_hide(BMP *bmp, const unsigned char *payload_buffer, const size_t payload_size) {
+    const size_t max_amount_bytes = bmp->data_size;
+    const size_t required_amount_bytes = (payload_size * STEGOBMP_LSBI_BYTES_PER_PAYLOAD) + STEGOBMP_LSBI_CONTROL_BYTES;
+
+    if (required_amount_bytes > max_amount_bytes) {
+        printf("Error: BMP does not have enough space to hide the payload\n");
+        return 1;
+    }
+
+    size_t bmp_byte_index = 0;
+
+    for (int i = 3; i >= 0; i--) {
+        const unsigned char control_bit = (STEGOBMP_LSBI_CONTROL_PATTERN >> i) & STEGOBMP_LSB1_BIT_MASK_1;
+        bmp->data[bmp_byte_index] = (bmp->data[bmp_byte_index] & STEGOBMP_LSB1_MASK) | control_bit;
+        bmp_byte_index++;
+    }
+
+    for (size_t payload_index = 0; payload_index < payload_size; payload_index++) {
+        for (int bit_index = STEGOBMP_LSBI_MOST_SIGNIFICANT_BIT; bit_index >= 0; bit_index--) {
+            const unsigned char bit = payload_buffer[payload_index] >> bit_index & STEGOBMP_LSBI_BIT_MASK_1;
+            const unsigned char bmp_msb = (bmp->data[bmp_byte_index] >> STEGOBMP_LSBI_MOST_SIGNIFICANT_BIT) & STEGOBMP_LSBI_BIT_MASK_1;
+            const unsigned char bit_to_hide = bit ^ bmp_msb;
+
+            bmp->data[bmp_byte_index] = (bmp->data[bmp_byte_index] & STEGOBMP_LSBI_MASK) | bit_to_hide;
+            bmp_byte_index++;
+        }
+    }
+
     return 0;
 }
