f92831d84a3380b3e143b84fa86164cfd1d6e5be
MatiSapino <msapino@itba.edu.ar>
2025-10-01 21:54:00 -0300

    Fix: LSB4 method

--- src/stegobmp.c
+++ src/stegobmp.c
@@ -9,6 +9,8 @@
 
 static unsigned char *build_payload_buffer(const char *input_filename, size_t *payload_size, char **payload_extension);
 static int lsb1_hide(BMP *bmp, const unsigned char *payload_buffer, size_t payload_size);
+static int lsb4_hide(BMP *bmp, const unsigned char *payload_buffer, size_t payload_size);
+static int lsbI_hide(BMP *bmp, const unsigned char *payload_buffer, size_t payload_size);
 
 int hide_file_in_bmp(const char *input_filename, BMP *bmp, const char *output_bmp_filename, const char *steganography_method, const char *encryption_method, const char *encryption_mode, const char *password) {
     size_t payload_size;
@@ -22,6 +24,20 @@ int hide_file_in_bmp(const char *input_filename, BMP *bmp, const char *output_bm
     if (strcmp(steganography_method, STEGOBMP_LSB1_METHOD) == 0) {
         if (lsb1_hide(bmp, payload_buffer, payload_size)) {
             printf("Error: Could not hide payload using LSB1\n");
+            free(payload_buffer);
+            free(payload_extension);
+            return 1;
+        }
+    } else if (strcmp(steganography_method, STEGOBMP_LSB4_METHOD) == 0) {
+        if (lsb4_hide(bmp, payload_buffer, payload_size)) {
+            printf("Error: Could not hide payload using LSB4\n");
+            free(payload_buffer);
+            free(payload_extension);
+            return 1;
+        }
+    } else if (strcmp(steganography_method, STEGOBMP_LSBI_METHOD) == 0) {
+        if (lsbI_hide(bmp, payload_buffer, payload_size)) {
+            printf("Error: Could not hide payload using LSBI\n");
             free(payload_buffer);
             free(payload_extension);
             return 1;
@@ -86,7 +102,7 @@ static unsigned char *build_payload_buffer(const char *input_filename, size_t *p
 
 static int lsb1_hide(BMP *bmp, const unsigned char *payload_buffer, const size_t payload_size) {
     const size_t max_amount_bytes = bmp->data_size;
-    const size_t required_amount_bytes = payload_size * STEGOBMP_BYTES_PER_BYTE;
+    const size_t required_amount_bytes = payload_size * STEGOBMP_LSB1_BYTES_PER_PAYLOAD;
 
     if (required_amount_bytes > max_amount_bytes) {
         printf("Error: BMP does not have enough space to hide the payload\n");
@@ -97,11 +113,40 @@ static int lsb1_hide(BMP *bmp, const unsigned char *payload_buffer, const size_t
 
     for (size_t payload_index = 0; payload_index < payload_size; payload_index++) {
         for (int bit_index = 7; bit_index >= 0; bit_index--) {
-            const unsigned char bit = payload_buffer[payload_index] >> bit_index & STEGOBMP_BIT_MASK_1;
+            const unsigned char bit = payload_buffer[payload_index] >> bit_index & STEGOBMP_LSB1_BIT_MASK_1;
             bmp->data[bmp_byte_index] = bmp->data[bmp_byte_index] & STEGOBMP_LSB1_MASK | bit;
             bmp_byte_index++;
         }
     }
 
+    return 0;
+}
+
+static int lsb4_hide(BMP *bmp, const unsigned char *payload_buffer, const size_t payload_size) {
+    const size_t max_amount_bytes = bmp->data_size;
+    const size_t required_amount_bytes = payload_size * STEGOBMP_LSB4_BYTES_PER_PAYLOAD;
+
+    if (required_amount_bytes > max_amount_bytes) {
+        printf("Error: BMP does not have enough space to hide the payload\n");
+        return 1;
+    }
+
+    size_t bmp_byte_index = 0;
+
+    for (size_t payload_index = 0; payload_index < payload_size; payload_index++) {
+        const unsigned char payload_high_nibble = payload_buffer[payload_index] >> STEGOBMP_LSB4_NIBBLE_SIZE_BITS & STEGOBMP_LSB4_BIT_MASK_4;
+        const unsigned char payload_low_nibble = payload_buffer[payload_index] & STEGOBMP_LSB4_BIT_MASK_4;
+
+        bmp->data[bmp_byte_index] = bmp->data[bmp_byte_index] & STEGOBMP_LSB4_MASK | payload_high_nibble;
+        bmp_byte_index++;
+
+        bmp->data[bmp_byte_index] = bmp->data[bmp_byte_index] & STEGOBMP_LSB4_MASK | payload_low_nibble;
+        bmp_byte_index++;
+    }
+
+    return 0;
+}
+
+static int lsbI_hide(BMP *bmp, const unsigned char *payload_buffer, size_t payload_size) {
     return 0;
 }
